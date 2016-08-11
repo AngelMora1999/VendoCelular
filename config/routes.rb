254b926c0123e29df18a00979c8e878ce8a3a915
Angel Mora <eduardo.aemc@gmail.com>
2016-08-11 18:47:04 -0500

    Login with Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :ads
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers:{
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
 
   authenticated :user do
     root 'home#index'
